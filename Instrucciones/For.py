52baae4b964c18b7d2c47e4de6c87632f130874a
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2021-06-15 10:10:56 -0400

    corrigiendo el incremento
    :D

--- Instrucciones/For.py
+++ Instrucciones/For.py
@@ -18,31 +18,31 @@ class For(Instruccion):
         nuevaTabla = TablaSimbolos(table)       #NUEVO ENTORNO
         declaracion=self.declaracion.interpretar(tree, nuevaTabla)
         if isinstance(declaracion, Excepcion): return declaracion
+        while True:
 
-
-        #verifica que se cumpla la condicion
-        try:
-            condicion = self.condicion.interpretar(tree, nuevaTabla)
-            if isinstance(condicion, Excepcion): return condicion
-        except:
-            return condicion
+            #verifica que se cumpla la condicion
+            try:
+                condicion = self.condicion.interpretar(tree, nuevaTabla)
+                if isinstance(condicion, Excepcion): return condicion
+            except:
+                return condicion
             
-        if self.condicion.tipo == TIPO.BOOLEANO:
-
-            if bool(condicion) == True:   # VERIFICA SI ES VERDADERA LA CONDICION
-
-                for instruccion in self.instrucciones:
-                    result = instruccion.interpretar(tree, nuevaTabla) #EJECUTA INSTRUCCION ADENTRO DEL IF
-                    if isinstance(result, Excepcion) :
-                        tree.getExcepciones().append(result)
-                        tree.updateConsola(result.toString())
-                    #SI HAY UN BREAK SALE DEL CICLO FOR
-                    if isinstance(result, Break): return None
-            else:
-                break
+            if self.condicion.tipo == TIPO.BOOLEANO:
                 
-        else:
-            return Excepcion("Semantico", "Tipo de dato no booleano en FOR.", self.fila, self.columna)
-        #ACTUALIZA LA VARIABLE
-        actualiza=self.actualiza.interpretar(tree, nuevaTabla)
-        if isinstance(actualiza, Excepcion): return actualiza
+                if bool(condicion) == True:   # VERIFICA SI ES VERDADERA LA CONDICION
+
+                    for instruccion in self.instrucciones:
+                        result = instruccion.interpretar(tree, nuevaTabla) #EJECUTA INSTRUCCION ADENTRO DEL IF
+                        if isinstance(result, Excepcion) :
+                            tree.getExcepciones().append(result)
+                            tree.updateConsola(result.toString())
+                        #SI HAY UN BREAK SALE DEL CICLO FOR
+                        if isinstance(result, Break): return None
+                else:
+                    break
+                
+            else:
+                return Excepcion("Semantico", "Tipo de dato no booleano en FOR.", self.fila, self.columna)
+            #ACTUALIZA LA VARIABLE
+            actualiza=self.actualiza.interpretar(tree, nuevaTabla)
+            if isinstance(actualiza, Excepcion): return actualiza
