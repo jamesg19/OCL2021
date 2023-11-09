12be94489a4b96e349350e7d93f8ee875d4b5d9d
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2023-11-09 12:26:52 -0600

    Update message

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OCL2021',
+    version='0.0.2',
+    packages=['ply'],
+    url='',
+    license='',
+    author='james',
+    author_email='james2000@hotmail.es',
+    description=''
+)
